dfb16eacf51b62e0559b9a0c35fd5c62c0a65ba0
joelleforestier <59805791+joelleforestier@users.noreply.github.com>
2022-03-20 19:03:32 -0400

    Version 1.0

--- R/geoGuess.R
+++ R/geoGuess.R
@@ -4,7 +4,7 @@
 #'
 #' @usage geoGuess()
 #'
-#' @param ct_code To pick up where you left off with a specific country or territory at some other time, optinally provide the CT Code corresponding to the country or territory you were guessing. The CT Code is provided at the start of each new round of the game.
+#' @param ct_code To pick up where you left off with a specific country or territory at some other time, optionally provide the CT Code corresponding to the country or territory you were guessing. The CT Code is provided at the start of each new round of the game.
 #'
 #' @author Joel Le Forestier (joel.leforestier@@mail.utoronto.ca)
 #'
@@ -29,25 +29,25 @@ geoGuess <- function(ct_code = 0) {
 
   #Display country and code
   plot(magick::image_read(paste0("https://raw.githubusercontent.com/teuteuf/worldle/37fd8f9c7cf44f7c05a8fac29384f507771145a7/public/images/countries/", tolower(countrydat$country[countrydat$name == country]), "/vector.svg")))
-  message(paste0("Country Code: "), ct_code)
+  message(paste0("CT Code: "), ct_code)
 
   #Ask for user guess
-  guess <- readline(prompt = "Guess a country: ")
+  guess <- readline(prompt = "Guess a country or territory: ")
 
   #Gve user feedback and another chance to guess
-  if(guess != country) {
-    while(guess != country) {
+  if (tolower(guess) != tolower(country)) {
+    while(tolower(guess) != tolower(country) & tolower(guess) != "i give up") {
 
-      if(!guess %in% countrydat$name) {
-        while(!guess %in% countrydat$name) {
-          message("That country is not in the dataset. Double check your spelling or try another country.")
-          guess <- readline(prompt = "Guess a country: ")
+      if(!tolower(guess) %in% tolower(countrydat$name)) {
+        while(!tolower(guess) %in% tolower(countrydat$name)) {
+          message("That country or territory is not in the dataset (many aren't!). Double check your spelling or try another country or territory.")
+          guess <- readline(prompt = "Guess a country or territory: ")
         }
       }
 
       #Calculate distance from correct country
-      latdiff <- countrydat$latitude[countrydat$name == guess] - countrydat$latitude[countrydat$name == country]
-      longdiff <- countrydat$longitude[countrydat$name == guess] - countrydat$longitude[countrydat$name == country]
+      latdiff <- countrydat$latitude[tolower(countrydat$name) == tolower(guess)] - countrydat$latitude[tolower(countrydat$name) == tolower(country)]
+      longdiff <- countrydat$longitude[tolower(countrydat$name) == tolower(guess)] - countrydat$longitude[tolower(countrydat$name) == tolower(country)]
 
       #Prepare feedback
       if(latdiff > 0) {
@@ -72,15 +72,21 @@ geoGuess <- function(ct_code = 0) {
         longdir <- "east or west"
       }
 
-      attempt <- attempt + 1
+      attempt <- attempt + .5
 
       #Provide feedback and solicit new guess
-      mtext(text = "×", side = 2, adj = 1, col="red", line = 1 - attempt, cex = 2)
-
+      mtext(text = "_", side = 2, adj = 1, col="red", line = 1 - attempt, cex = 2)
       guess <- readline(prompt = paste0("Your guess was ", round(abs(latdiff), 2), "° too far ", latdir, " and ", round(abs(longdiff), 2), "° too far ", longdir, ". Guess again: "))
     }
   }
-
-  #Provide feebdack
-  message("Great job!")
+  
+  #Check if they gave up
+  if (tolower(guess) == "i give up") {
+    message(paste0("The answer was: ", country))
+  } else {
+    
+    #Provide feebdack if they got it right
+    mtext(text = "_", side = 2, adj = 1, col="green", line = 1 - (attempt + .5), cex = 2)
+    message("🎉🎉🎉 Great job!🎉🎉🎉")
+  }
 }
--- R/zzz.R
+++ R/zzz.R
@@ -5,11 +5,13 @@ GUESS THE COUNTRY OR TERRITORY
 ##############################
 
 INSTRUCTIONS:
-1. Run the geoGuess() function to begin the game. Guess the name of the country or territory that appears in the plot viewer by typing your guess in the console.
-2. Be mindful of spelling and capitalization. If you are unsure of how a country or territory is spelled in the dataset, or whether a country or territory is included in the dataset (as not all countries in the world are represented in the data and not all the entries in the dataset are countries), run the ctList() function to view the complete list of countries and territories as they are recorded in the dataset.
-3. To end the game (or the round of the game) without correctly guessing the country or territory, press escape.
-4. If you want to end the game and continue guessing the same country or territory at another time, record the CT Code provided to you at the start of the game and add it to the ct_code argument when you run the geoGuess function.
-5. You have unlimited guesses. The number of incorrect guesses are recorded on on the image of the country or territory.
+1. Run the geoGuess() function to begin the game.
+2. Guess the name of the country or territory by typing your guess in the console.
+3. Not all countries in the world are represented in the dataset, and not all entries represented in the dataset are countries.
+   Run the ctList() function to view the list of countries and territories in the dataset.
+4. To end the game (or the round of the game) without correctly guessing the country or territory, press escape.
+4. If you want to continue a previous game, use the country's CT Code for the ct_code argument.
+5. You have unlimited guesses. Your guesses are tallied on on the image of the country or territory.
 
 CREDITS:
 Country data adapted from: https://developers.google.com/public-data/docs/canonical/countries_csv (CC BY 4.0)
